8afd3e12dafa6ac3d43f54f9da05378717a7097e
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-07 16:59:32 +0300

    refactor: remove unnecessary built-in vite files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Button } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div

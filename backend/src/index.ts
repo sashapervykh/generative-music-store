5cd31b3a341c5ac6fe7b6b7092e7027018641488
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-08 16:34:01 +0300

    feat: add basic controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,16 +3,14 @@ import cors from "cors";
 import { testServer } from "./test.js";
 import dotenv from "dotenv";
 import { ROUTES } from "./constants/routes.js";
+import { generatorController } from "./controllers/generator.controller.js";
 
 dotenv.config();
 const port = Number(process.env.PORT) || 3000;
 const app = express();
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
-app.get(ROUTES.GENERATE, (request, response) => {
-  const test = testServer();
-  response.send({ message: `${test}: Express + TypeScript Server` });
-});
+app.get(ROUTES.GENERATE, generatorController.generateData);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);

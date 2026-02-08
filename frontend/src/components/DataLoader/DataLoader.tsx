6964b17082adfa6558af03664f0c86f21bee201e
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-08 20:40:37 +0300

    feat: add debounce and abort controller to prevent excessive requests

--- frontend/src/components/DataLoader/DataLoader.tsx
+++ frontend/src/components/DataLoader/DataLoader.tsx
@@ -5,19 +5,31 @@ export function DataLoader({ children }: { children: ReactNode }) {
   const { language, seed, likes } = useDataConfig();
 
   useEffect(() => {
-    async function requestSongsData() {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const timeoutId = setTimeout(async () => {
       try {
         const response = await fetch(
           `${import.meta.env.VITE_API_URL}?language=${language}&seed=${seed}&likes=${likes}`,
+          { signal },
         );
+
+        if (!response.ok) {
+          throw new Error("Failed request");
+        }
+
         const songsData = await response.json();
         console.log(songsData);
-      } catch {
-        console.error("Some Error during request");
+      } catch (error) {
+        console.error("Request error:", error);
       }
-    }
+    }, 400);
 
-    requestSongsData();
+    return () => {
+      clearTimeout(timeoutId);
+      controller.abort();
+    };
   }, [language, seed, likes]);
 
   return <>{children}</>;

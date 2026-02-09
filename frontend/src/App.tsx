f9f53cf7fcf9e55694e2a65f0636b4e5b85b8574
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-09 18:35:24 +0300

    feat: show table with data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { AppTitle } from "./components/AppTitle/AppTitle";
 import { DataLoader } from "./components/DataLoader/DataLoader";
 import { LocaleLoader } from "./components/LocaleLoader/LocaleLoader";
+import { TableView } from "./components/TableView/TableView";
 import { Toolbar } from "./components/Toolbar/Toolbar";
 import { DataConfigProvider } from "./providers/DataConfigProvider";
 import { LocalesProvider } from "./providers/LocalesProvider";
@@ -16,6 +17,7 @@ function App() {
               <DataLoader>
                 <AppTitle />
                 <Toolbar />
+                <TableView />
               </DataLoader>
             </LocaleLoader>
           </DataConfigProvider>
---
+++ frontend/src/components/TableView/TableView.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Table } from "antd";
+import type { TableColumnsType } from "antd";
+import { useSongs } from "../../hooks/useSongs";
+
+interface DataType {
+  key: React.Key;
+  id: number;
+  song: string;
+  artist: string;
+  album: string;
+  genre: string;
+}
+
+const columns: TableColumnsType<DataType> = [
+  { title: "#", dataIndex: "id" },
+  { title: "Song", dataIndex: "song" },
+  { title: "Artist", dataIndex: "artist" },
+  { title: "Album", dataIndex: "album" },
+  { title: "Genre", dataIndex: "genre" },
+];
+
+export function TableView() {
+  const { songs } = useSongs();
+  const dataToDisplay = songs.map((song) => ({
+    key: song.id,
+    id: song.id,
+    song: song.title,
+    artist: song.artist,
+    album: song.album,
+    genre: song.genre,
+  }));
+
+  return (
+    <Table<DataType>
+      columns={columns}
+      expandable={{
+        expandedRowRender: () => <p>To be added later</p>,
+      }}
+      dataSource={dataToDisplay}
+    />
+  );
+}

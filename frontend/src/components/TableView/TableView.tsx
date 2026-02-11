0e222783418086f34f46a5126458820501411ce1
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-11 21:09:23 +0300

    feat: adjust styles for expendable card

--- frontend/src/components/TableView/TableView.tsx
+++ frontend/src/components/TableView/TableView.tsx
@@ -3,6 +3,7 @@ import { Table } from "antd";
 import type { TableColumnsType, TablePaginationConfig } from "antd";
 import { useSongs } from "../../hooks/useSongs";
 import { useDataConfig } from "../../hooks/useDataConfig";
+import { LikeOutlined } from "@ant-design/icons";
 
 interface DataType {
   key: React.Key;
@@ -47,7 +48,7 @@ export function TableView() {
 
   return (
     <Table<DataType>
-      className="capitalize"
+      className="capitalize w-[90%] m-auto pt-8"
       columns={columns}
       pagination={{
         current: page,
@@ -59,11 +60,21 @@ export function TableView() {
         expandedRowRender: (record) => {
           console.log(record.image);
           return (
-            <div>
-              <img src={record.image}></img>
-              <p>{record.review}</p>
-              <audio src={record.music} controls></audio>
-              <p>{record.likes}</p>
+            <div className="grid grid-cols-[200px_auto] gap-4 justify-center">
+              <div className="flex flex-col">
+                <img className="flex m-auto rounded-xl" src={record.image} />
+                <p className="flex m-[0.2rem_auto_0px_auto] gap-1.5">
+                  <LikeOutlined /> {record.likes}
+                </p>
+              </div>
+              <div className="flex flex-col normal-case justify-center p-2rem">
+                <p>{record.review}</p>
+                <audio
+                  className="m-[1.5rem_auto_0px_auto]"
+                  src={record.music}
+                  controls
+                ></audio>
+              </div>
             </div>
           );
         },
